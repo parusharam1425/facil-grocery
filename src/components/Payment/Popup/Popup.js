764f3738976572b4fa2333f8b97e609e0dde2aeb
Parusharam <parusharam4888@gmail.com>
2023-12-15 14:03:25 +0530

    responsive done

--- src/components/Payment/Popup/Popup.js
+++ src/components/Payment/Popup/Popup.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
-import classes from './Popup.module.css'
+import React, { useState } from 'react';
 import closeimg from '../../../assets/Payment/close.jpg';
 import visa from '../../../assets/Payment/visa.jpg';
 import mastercard from '../../../assets/Payment/master.jpg';
 import americanexpress from '../../../assets/Payment/american.jpg';
 import bajaj from '../../../assets/Payment/bajaj.jpg';
 import rupay from '../../../assets/Payment/rupay.jpg';
+import './Popup.css'
 const PopUp = () => {
   const [isPopupOpen, setPopupOpen] = useState(true);
-  
+
   const [cardData, setCardData] = useState({
     cardNumber: '',
     nickname: '',
@@ -21,9 +21,7 @@ const PopUp = () => {
   };
 
   const handleEnterCardDetails = () => {
-    // Store data in local storage
     localStorage.setItem('cardData', JSON.stringify(cardData));
-    // Optionally, you can also close the popup here
     setPopupOpen(false);
   };
 
@@ -33,74 +31,73 @@ const PopUp = () => {
       [field]: e.target.value,
     });
   };
+
   return (
     <>
       {isPopupOpen && (
-        <div className={classes.card_container}>
-          <div className={classes.card_header}>
-            <h1>Enter card details</h1>
-            <img src={closeimg} alt='' className={classes.close_img} onClick={handleClosePopup} />
-          </div>
-          <div className={classes.card_body}>
+        <div className='container'>
 
-          <div className={classes.col1}>
-              <div className={classes.input_div}>
-                <label>Card number</label>
+        
+        <div className="card_container">
+          <div className="card_header">
+            <h1 style={{fontSize:"20px", fontWeight:"600"}}>Enter card details</h1>
+            <img src={closeimg} alt='' className="close_img" onClick={handleClosePopup} />
+          </div>
+          <div className='row'>
+            <div className="col-12 col-md-6 col1">
+              <div className="input_div">
+                <label className='label'>Card number</label>
                 <input
                   type='number'
-                  className={classes.popup_input}
+                  className="popup_input_card popup_input"
                   onChange={(e) => handleInputChange(e, 'cardNumber')}
                 />
               </div>
-
-              <div className={`${classes.inputDiv} ${classes.inputDiv2}`}>
-                <label>Nickname</label>
+              <div className="inputDiv inputDiv2">
+                <label className='label'>Nickname</label>
                 <input
                   type='text'
-                  className={classes.popup_input}
+                  className="popup_input_nick popup_input"
                   onChange={(e) => handleInputChange(e, 'nickname')}
                 />
               </div>
-
-              <div className={classes.popup_input_date}>
-                <label>Expiry date</label>
+              <div className="popup_input_date">
+                <label className='label'>Expiry date</label>
+                
                 <input
                   type='number'
-                  className={classes.date}
+                  className="date"
                   onChange={(e) => handleInputChange(e, 'expiryMonth')}
                 />
                 <input
                   type='number'
-                  className={classes.date}
+                  className="date"
                   onChange={(e) => handleInputChange(e, 'expiryYear')}
                 />
               </div>
-        </div>
+            </div>
+            
+              <div className='col-12 col-md-6'>
 
-            <div className={classes.col2}>
-              <p>Please ensure that you enable your card
-                for online payments from your bank's app
-              </p>
-              <div className={classes.cards_img}>
+              <p className='p-3'>Please ensure that you enable your card for online payments from your bank's app</p>
+              <div className="cards_img">
                 <img src={visa} alt='' />
                 <img src={mastercard} alt='' />
                 <img src={americanexpress} alt='' />
                 <img src={bajaj} alt='' />
                 <img src={rupay} alt='' />
               </div>
-            </div>
-
           </div>
-          <div className={classes.card_footer}>
-            <button onClick={handleClosePopup}>Cancel</button>
-            <button onClick={handleEnterCardDetails}>Enter card details</button>
-
+          </div>
+          <div className="card_footer">
+            <button className='btn btn-danger ' onClick={handleClosePopup}>Cancel</button>
+            <button className='btn btn-secondary' onClick={handleEnterCardDetails}>Enter card details</button>
           </div>
         </div>
+        </div>
       )}
-
     </>
-  )
+  );
 }
 
-export default PopUp
+export default PopUp;
